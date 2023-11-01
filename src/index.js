c8b2e9f4a4185e869436496c52440401bbb2d40c
Vas2306 <vasilisa230679@gmail.com>
2023-11-01 21:01:51 +0100

    d

--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import './css/03-home-11.css';
 import './js/search-images/01-pokemon';
 import './js/search-images/02-cat';
 import './js/search-images/03-home-11';
+import './js/search-images/load-more-btn';
+import './js/search-images/news-service';
+
 
