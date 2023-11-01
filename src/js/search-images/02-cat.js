5b24033f3586a9f19c1b558fada168bc3b705492
Vas2306 <vasilisa230679@gmail.com>
2023-11-01 21:18:25 +0100

    f

--- src/js/search-images/02-cat.js
+++ src/js/search-images/02-cat.js
@@ -1,7 +1,75 @@
 import articlesTpl from '../templates/cat.hbs';
-import NewApiService from './news-service';
-console.log(NewApiService);
-import LoadMoreBtn from './load-more-btn';
+const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
+const BASE_URL = `https://newsapi.org/v2`;
+const options = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
+class NewApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchArticles() {
+    console.log(this);
+
+    const url = `${BASE_URL}/everything?q=${this.searchQuery}&pageSize=5&page=${this.page}&language
+=ru`;
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(({articles}) => {
+        this.page += 1;
+        return articles;
+      });
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+class LoadMoreBtn {
+  constructor({ selector, hidden = false }) {
+    this.refs = this.getRefs(selector);
+
+    hidden && this.hide();
+  }
+
+  getRefs(selector) {
+    const refs = {};
+    refs.button = document.querySelector(selector);
+    refs.label = refs.button.querySelector('.label');
+    refs.spinner = refs.button.querySelector('.spinner');
+
+    return refs;
+  }
+
+  enable() {
+    this.refs.button.disabled = false;
+    this.refs.label.textContent = 'Показать ещё';
+    this.refs.spinner.classList.add('is-hidden');
+  }
+
+  disable() {
+    this.refs.button.disabled = true;
+    this.refs.label.textContent = 'Загружаем...';
+    this.refs.spinner.classList.remove('is-hidden');
+  }
+
+  show() {
+    this.refs.button.classList.remove('is-hidden');
+  }
+
+  hide() {
+    this.refs.button.classList.add('is-hidden');
+  }
+}
+
 
 const refs = {
   seachForms: document.querySelector('.js-search-form'),
